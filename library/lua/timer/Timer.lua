d3997d7312bfced4d86d2a08a72087842bf8eb02
jaronho <892134825@qq.com>
2017-11-28 18:59:53 +0800

    lua library Timer

--- library/lua/timer/Timer.lua
+++ library/lua/timer/Timer.lua
@@ -3,157 +3,162 @@
 -- Date:	2014-04-14
 -- Brief:	timer
 ----------------------------------------------------------------------
-local mTimerList = {}
-----------------------------------------------------------------------
-local function insertTimer(tm)
-	table.insert(mTimerList, tm)
-end
-----------------------------------------------------------------------
-local function removeTimer(tm)
-	for key, val in pairs(mTimerList) do
-		if tm == val then
-			table.remove(mTimerList, key)
-			return
-		end
-	end
-end
-----------------------------------------------------------------------
--- called every frame
-function UpdateTimer(currentTime)
-	for _, val in pairs(mTimerList) do
-		if ("table" == type(val) or "userdata" == type(val)) and "function" == type(val.update) then
-			val:update(currentTime)
-		end
-	end
-end
-----------------------------------------------------------------------
--- clear timer list
-function ClearTimer()
-	mTimerList = {}
-end
-----------------------------------------------------------------------
--- create a timer
-function CreateTimer(interval, count, runCF, overCF, target, param)
+function CreateTimerManager()
 	-- private member variables
-	assert(interval > 0, "interval <= 0")
-	local mInterval = interval			-- interval duration in milliseconds
-	local mTotalCount = count			-- number of intervals, if count <= 0, timer will repeat forever
-	local mCurrentCount = 0				-- current interval count
-	local mStartTime = 0				-- start time for the current interval in milliseconds
-	local mRunning = false				-- status of the timer
-	local mIsPause = false				-- is timer paused
-	local mRunCallFunc = runCF			-- called when current count changed
-	local mOverCallFunc = overCF		-- called when timer is complete
-	local mTarget = target				-- callback target
-	local mParam = param				-- parameter
-	local tm = {}
+	local mTimerList = {}
+	local timerManager = {}
+	-- private methods
+	local function insertTimer(tm)
+		for key, val in pairs(mTimerList) do
+			if tm == val then
+				return
+			end
+		end
+		table.insert(mTimerList, tm)
+	end
+	local function removeTimer(tm)
+		for key, val in pairs(mTimerList) do
+			if tm == val then
+				table.remove(mTimerList, key)
+				return
+			end
+		end
+	end
 	-- public methods
-	function tm:update(currentTime)
-		if not mRunning then
-			return
+	function timerManager:updateTimer(currentTime)
+		for _, val in pairs(mTimerList) do
+			if ("table" == type(val) or "userdata" == type(val)) and "function" == type(val.update) then
+				val:update(currentTime)
+			end
 		end
-		if mIsPause or currentTime < mStartTime then
-			mStartTime = currentTime
-			return
-		end
-		if mTotalCount <= 0 or mCurrentCount < mTotalCount then
-			local deltaTime = math.abs(currentTime - mStartTime)
-			if deltaTime >= mInterval then
-				local runCount = math.floor(deltaTime/mInterval)
-				mCurrentCount = mCurrentCount + runCount
+	end
+	function timerManager:clearTimer()
+		mTimerList = {}
+	end
+	function timerManager:createTimer(interval, count, runCF, overCF, target, param)
+		-- private member variables
+		assert(interval > 0, "interval <= 0")
+		local mInterval = interval			-- interval duration in milliseconds
+		local mTotalCount = count			-- number of intervals, if count <= 0, timer will repeat forever
+		local mCurrentCount = 0				-- current interval count
+		local mStartTime = 0				-- start time for the current interval in milliseconds
+		local mRunning = false				-- status of the timer
+		local mIsPause = false				-- is timer paused
+		local mRunCallFunc = runCF			-- called when current count changed
+		local mOverCallFunc = overCF		-- called when timer is complete
+		local mTarget = target				-- callback target
+		local mParam = param				-- parameter
+		local tm = {}
+		-- public methods
+		function tm:update(currentTime)
+			if not mRunning then
+				return
+			end
+			if mIsPause or currentTime < mStartTime then
 				mStartTime = currentTime
-				if "function" == type(mRunCallFunc) then
-					if "table" == type(mTarget) or "userdata" == type(mTarget) then
-						mRunCallFunc(mTarget, self, runCount)
-					else
-						mRunCallFunc(self, runCount)
+				return
+			end
+			if mTotalCount <= 0 or mCurrentCount < mTotalCount then
+				local deltaTime = math.abs(currentTime - mStartTime)
+				if deltaTime >= mInterval then
+					local runCount = math.floor(deltaTime/mInterval)
+					mCurrentCount = mCurrentCount + runCount
+					mStartTime = currentTime
+					if "function" == type(mRunCallFunc) then
+						if "table" == type(mTarget) or "userdata" == type(mTarget) then
+							mRunCallFunc(mTarget, self, runCount)
+						else
+							mRunCallFunc(self, runCount)
+						end
 					end
 				end
-			end
-		else
-			self:stop(true)
-		end
-	end
-	function tm:start(currentTime, executeFlag)
-		if mRunning then
-			return
-		end
-		mRunning = true
-		mIsPause = false
-		mCurrentCount = 0
-		mStartTime = currentTime
-		if "function" == type(mRunCallFunc) and executeFlag then
-			if "table" == type(mTarget) or "userdata" == type(mTarget) then
-				mRunCallFunc(mTarget, self, 1)
 			else
-				mRunCallFunc(self, 1)
+				self:stop(true)
 			end
 		end
-		insertTimer(self)
-	end
-	function tm:stop(executeFlag)
-		if not mRunning then
-			return
+		function tm:start(currentTime, executeFlag)
+			if mRunning then
+				return
+			end
+			mRunning = true
+			mIsPause = false
+			mCurrentCount = 0
+			mStartTime = currentTime
+			if "function" == type(mRunCallFunc) and executeFlag then
+				if "table" == type(mTarget) or "userdata" == type(mTarget) then
+					mRunCallFunc(mTarget, self, 1)
+				else
+					mRunCallFunc(self, 1)
+				end
+			end
+			insertTimer(self)
 		end
-		mRunning = false
-		mIsPause = true
-		if "function" == type(mOverCallFunc) and executeFlag then
-			if "table" == type(mTarget) or "userdata" == type(mTarget) then
-				mOverCallFunc(mTarget, self)
-			else
-				mOverCallFunc(self)
+		function tm:stop(executeFlag)
+			if not mRunning then
+				return
+			end
+			removeTimer(self)
+			mRunning = false
+			mIsPause = true
+			if "function" == type(mOverCallFunc) and executeFlag then
+				if "table" == type(mTarget) or "userdata" == type(mTarget) then
+					mOverCallFunc(mTarget, self)
+				else
+					mOverCallFunc(self)
+				end
 			end
 		end
-		removeTimer(self)
+		function tm:resume()
+			mIsPause = false
+		end
+		function tm:pause()
+			mIsPause = true
+		end
+		function tm:getInterval()
+			return mInterval
+		end
+		function tm:setInterval(interval)
+			assert(interval > 0, "interval <= 0")
+			mInterval = interval
+		end
+		function tm:getTotalCount()
+			return mTotalCount
+		end
+		function tm:setTotalCount(count)
+			mTotalCount = count
+		end
+		function tm:getCurrentCount()
+			return mCurrentCount
+		end
+		function tm:isRunning()
+			return mRunning
+		end
+		function tm:setRunHandler(runCF)
+			mRunCallFunc = runCF
+		end
+		function tm:setOverHandler(overCF)
+			mOverCallFunc = overCF
+		end
+		function tm:getParam()
+			return mParam
+		end
+		function tm:setParam(param)
+			mParam = param
+		end
+		return tm
 	end
-	function tm:resume()
-		mIsPause = false
-	end
-	function tm:pause()
-		mIsPause = true
-	end
-	function tm:getInterval()
-		return mInterval
-	end
-	function tm:setInterval(interval)
-		assert(interval > 0, "interval <= 0")
-		mInterval = interval
-	end
-	function tm:getTotalCount()
-		return mTotalCount
-	end
-	function tm:setTotalCount(count)
-		mTotalCount = count
-	end
-	function tm:getCurrentCount()
-		return mCurrentCount
-	end
-	function tm:isRunning()
-		return mRunning
-	end
-	function tm:setRunHandler(runCF)
-		mRunCallFunc = runCF
-	end
-	function tm:setOverHandler(overCF)
-		mOverCallFunc = overCF
-	end
-	function tm:getParam()
-		return mParam
-	end
-	function tm:setParam(param)
-		mParam = param
-	end
-	return tm
+	return timerManager
 end
 ----------------------------------------------------------------------
 -- test code
 --[[
+local TimeManager = CreateTimerManager();
 local function timer1_CF1(tm, runCount)
 	tm:setParam("count_"..tm:getCurrentCount())
 end
 local function timer1_CF2(tm)
 end
-local timer1 = CreateTimer(1, 0, timer1_CF1, timer1_CF2)
+local timer1 = TimeManager.createTimer(1, 0, timer1_CF1, timer1_CF2);
 timer1:setParam("hahaha")
 timer1:start()
 ]]
