dba5f19fe816ddc3acc89231fcd643d197a3cd36
jaronho <892134825@qq.com>
2018-01-11 15:45:05 +0800

    c++ library Algorithm

---
+++ library/c++/Algorithm.cpp
@@ -0,0 +1,68 @@
+/******************************************************************************
+* Author:	jaron.ho
+* Date:		2018-01-11
+* Brief:	algorithm
+******************************************************************************/
+#include "Algorithm.h"
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//--------------------------------------------------------------------------
+unsigned int Algorithm::colorGrayscale(unsigned int red, unsigned int green, unsigned int blue) {
+    red = red > 255 ? 255 : red;
+    green = green > 255 ? 255 : green;
+    blue = blue > 255 ? 255 : blue;
+    return (red * 30 + green * 59 + blue * 11) / 100;
+}
+//--------------------------------------------------------------------------
+unsigned char* Algorithm::cryptoXOR(unsigned char* data, const unsigned char* key) {
+    if (!data || 0 == strcmp("", (char*)data) || !key || 0 == strcmp("", (char*)key)) {
+        return data;
+    }
+    unsigned int dataLength = strlen((char*)data);
+    unsigned int keyLength = strlen((char*)key);
+    for (unsigned int i=0; i< dataLength; ++i) {
+        data[i] = data[i] ^ key[i % keyLength];
+    }
+    return data;
+}
+//--------------------------------------------------------------------------
+unsigned int Algorithm::hashCode(const unsigned char* data) {
+    if (!data) {
+        return 0;
+    }
+    const unsigned char* end = data + strlen((char*)data);
+    unsigned int hash = 0;
+    for (hash = 0; data < end; ++data) {
+        hash *= 16777619;
+        hash ^= (unsigned int)toupper(*data);
+    }
+    return hash;
+}
+//--------------------------------------------------------------------------
+int Algorithm::mathRound(float num) {
+    return (int)(num + 0.5f);
+}
+//--------------------------------------------------------------------------
+double Algorithm::mathRand(double start, double end) {
+    static bool seedFlag = true;
+    if (seedFlag) {
+        seedFlag = false;
+        srand(unsigned(time(0)));
+    }
+    return start + (end - start)*rand()/(RAND_MAX);
+}
+//--------------------------------------------------------------------------
+bool Algorithm::mathProbability(unsigned int probability) {
+    if (0 == probability) {
+        return false;
+    }
+    unsigned int randRes = (unsigned int)mathRand(0, 100);	// init set rand range [0, 100)
+    if (0 == randRes) {	// make sure range is [1, 100]
+        randRes += 1;
+    }
+    return randRes <= probability;
+}
+//--------------------------------------------------------------------------
---
+++ library/c++/Algorithm.h
@@ -0,0 +1,58 @@
+/******************************************************************************
+* Author:	jaron.ho
+* Date:		2018-01-11
+* Brief:	algorithm
+******************************************************************************/
+#ifndef _ALGORITHM_H_
+#define _ALGORITHM_H_
+
+class Algorithm {
+public:
+    /*
+     * Brief:   calculate grayscale value
+     * Param:   red - red channel value
+     *          green - green channel value
+     *          blue - blue channel value
+     * Return: unsigned int
+     */
+    static unsigned int colorGrayscale(unsigned int red, unsigned int green, unsigned int blue);
+
+    /*
+     * Brief:	XOR crypto
+     * Param:	data - data
+     *          key - crypto key
+     * Return:	unsigned char*
+     */
+    static unsigned char* cryptoXOR(unsigned char* data, const unsigned char* key);
+
+    /*
+     * Brief:	get hash code
+     * Param:	data - any string
+     * Return:	unsigned int
+     */
+    static unsigned int hashCode(const unsigned char* data);
+
+    /*
+     * Brief:	math round
+     * Param:	num - number
+     * Return:	int
+     */
+    static int mathRound(float num);
+
+    /*
+     * Brief:	get random number, [start, end)
+     * Param:	start - start number
+     *          end - end number
+     * Return:	double
+     */
+    static double mathRand(double start, double end);
+
+    /*
+     * Brief:	get probability in a region
+     * Param:	probability - (0, 100], 5 represent 5% or 0.05
+     * Return:	bool
+     */
+    static bool mathProbability(unsigned int probability);
+};
+
+#endif	// _ALGORITHM_H_
