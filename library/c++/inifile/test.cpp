acd5cbe5c4466770ccbb8b0ff7e0a8292c9876b9
jaronho <892134825@qq.com>
2018-04-03 17:51:56 +0800

    library c++ IniFile

--- library/c++/inifile/test.cpp
+++ library/c++/inifile/test.cpp
@@ -1,24 +1,20 @@
-#include "ConfigINI.h"
 #include <iostream>
+#include "IniFile.h"
 
-int main() {
-    ConfigINI *ini = new ConfigINI("test.ini");
-    ini->setIntValue("section1", "intValue", 1);
-    ini->setFloatValue("section1", "floatValue", 0.1);
-    ini->setStringValue("section2", "stringValue", "hello, world");
-    ini->setBoolValue("section2", "boolValue", true);
-    ini->writeConfigFile();
+using namespace std;
 
-    int intValue = ini->getIntValue("section1", "intValue");
-    float floatValue = ini->getFloatValue("section1", "floatValue");
-    const char *stringValue = ini->getStringValue("section2", "stringValue");
-    bool boolValue = ini->getBoolValue("section2", "boolValue");
-
-    cout<<"[section1]-intValue:\t"<<intValue<<endl;
-    cout<<"[section1]-floatValue:\t"<<floatValue<<endl;
-    cout<<"[section2]-stringValue:\t"<<stringValue<<endl;
-    cout<<"[section2]-boolValue:\t"<<boolValue<<endl;
-
-    delete ini;
+int main()
+{
+    cout << "Hello World!" << endl;
+    IniFile ini;
+    int ret = ini.open("test.ini");
+    ini.setString("SERVER", "IP", "190.168.3.96");
+    ini.setInt("SERVER", "PORT", 5967);
+    ini.setString("HTTP", "POST", "http://www.baidu.com");
+    ini.setBool("HTTP", "AUTO", true);
+    ini.save();
+    std::string ip = ini.getString("SERVER", "IP");
+    int port = ini.getInt("SERVER", "PORT");
+    printf("%s === %d\n", ip.c_str(), port);
     return 0;
 }
