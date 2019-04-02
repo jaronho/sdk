e79db118ce421528c01d7f4e2169e25893c008ce
jaronho <892134825@qq.com>
2019-04-02 09:52:35 +0800

    library/c++/action

---
+++ library/c++/action/Action.cpp
@@ -0,0 +1,95 @@
+/**********************************************************************
+ * Author:	jaron.ho
+ * Date:    2019-04-01
+ * Brief:	动作(支持同步/异步),需要实现子类
+ **********************************************************************/
+#include "Action.h"
+#include <list>
+#inculde <mutex>
+#include <thread>
+
+static bool sInitGroupThread = true;
+static std::mutex sProcessMutex;
+static std::list<Action*> sProcessList;
+static std::mutex sFinishMutex;
+static std::list<Action*> sFinishList;
+
+static void aloneHandler(Action* act) {
+    if (act) {
+        act->onProcess();
+        sFinishMutex.lock();
+        sFinishList.push_back(act);
+        sFinishMutex.unlock();
+    }
+}
+
+static void groupHandler(void) {
+    while (true) {
+        Action* act = nullptr;
+        sProcessMutex.lock();
+        if (sProcessList.size() > 0) {
+            act = *(sProcessList.begin());
+            sProcessList.pop_front();
+        }
+        sProcessMutex.unlock();
+        if (act) {
+            act->onProcess();
+            sFinishMutex.lock();
+            sFinishList.push_back(act);
+            sFinishMutex.unlock();
+        }
+        usleep(10000);  /* 10毫秒 */
+    }
+}
+
+Action::Action(ACTION_FINISH_CALLBACK callback) {
+    mFinishCallback = callback;
+}
+
+void Action::syncRun(Action* act, bool autoDestroy) {
+    if (act) {
+        act->onProcess();
+        if (act->mFinishCallback) {
+            act->mFinishCallback(act);
+        }
+        if (autoDestroy) {
+            delete act;
+            act = nullptr;
+        }
+    }
+}
+
+void Action::asyncRun(Action* act, bool alone) {
+    if (act) {
+        if (alone) {
+            std::thread aloneThread(aloneHandler, act);
+            aloneThread.joinable();
+        } else {
+            if (sInitGroupThread) {
+                sInitGroupThread = false;
+                std::thread groupThread(groupHandler);
+                groupThread.joinable();
+            }
+            sProcessMutex.lock();
+            sProcessList.push_back(act);
+            sProcessMutex.unlock();
+        }
+    }
+}
+
+void Action::asyncListen(void) {
+    Action* act = nullptr;
+    sFinishMutex.lock();
+    if (sFinishList.size() > 0) {
+        act = *(sFinishList.begin());
+        sFinishList.pop_front();
+    }
+    sFinishMutex.unlock();
+    if (act) {
+        if (act->mFinishCallback) {
+            act->mFinishCallback(act);
+        }
+        delete act;
+        act = nullptr;
+    }
+}
---
+++ library/c++/action/Action.h
@@ -0,0 +1,43 @@
+/**********************************************************************
+ * Author:	jaron.ho
+ * Date:    2019-04-01
+ * Brief:	动作(支持同步/异步),需要实现子类
+ **********************************************************************/
+#ifndef _ACTION_H_
+#define _ACTION_H_
+
+/* 动作结束回调 */
+#define ACTION_FINISH_CALLBACK std::function<void(Action* act)>
+
+class Action {
+public:
+    Action(ACTION_FINISH_CALLBACK callback = nullptr);
+    virtual void onProcess(void) {}         /* 执行函数 */
+
+public:
+    /*
+     * Brief:	同步执行(在函数调用线程执行)
+     * Param:	autoDestroy - true.动作自动销毁,false.需要手动销毁动作
+     * Return:	void
+     */
+    static void syncRun(Action* act, bool autoDestroy = true);
+
+    /*
+     * Brief:	异步执行
+     * Param:	alone - true.在独立的线程中运行,false.在预设的线程中排队运行
+     * Return:	void
+     */
+    static void asyncRun(Action* act, bool alone = false);
+
+    /*
+     * Brief:	异步监听(在主线程中循环执行),若动作有回调函数,则回调函数将在主线程中被调用
+     * Param:	void
+     * Return:	void
+     */
+    static void asyncListen(void);
+
+private:
+    ACTION_FINISH_CALLBACK mFinishCallback;
+};
+
+#endif	/* _ACTION_H_ */
