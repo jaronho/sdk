964ec36d236a3070d517154459fac1dea419be47
jaronho <892134825@qq.com>
2019-05-31 09:35:57 +0800

    library/javascript/Curve.js

---
+++ library/javascript/Curve.js
@@ -0,0 +1,114 @@
+/***********************************************************************
+ ** Author:	jaron.ho
+ ** Date:	2019-04-13
+ ** Brief:	曲线
+ ***********************************************************************/
+.pragma library
+Qt.include("jhsdk/Formula.js")
+/*
+ * Brief:   创建曲线,由多条贝赛儿曲线组成
+ * Param:   points - 曲线所要经过的坐标点,两两坐标点形成单独的一条曲线,例如:[[0, 0], [10, 10], [20, 20]]
+ *          ratio - 平滑比率
+ * Return:  曲线对象
+ */
+function createCurve(points, ratio) {
+    var curve = {};
+    /* 更新坐标点 */
+    curve.update = function(points, ratio) {
+        this._points = points;
+        if ('number' === typeof(ratio)) {
+            this._ratio = ratio;
+        }
+        this._partions = Formula.smoothCurve(points, this._ratio);
+        this._lengths = [];
+        this._totalLength = 0;
+        for (var i = 0; i < this._partions.length; ++i) {
+            var partLen = Formula.bezierLength(this._partions[i], 2, 30);
+            this._lengths.push(partLen);
+            this._totalLength += partLen;
+        }
+    };
+    /* 清除 */
+    curve.clear = function() {
+        this._points = [];
+        this._partions = [];
+        this._lengths = [];
+        this._totalLength = 0;
+    };
+    /* 获取总长度 */
+    curve.getTotalLength = function() {
+        return this._totalLength;
+    };
+    /* 获取坐标点 */
+    curve.getPoint = function(t) {
+        var len = this._totalLength * t;
+        var sum = 0;
+        for (var i = 0; i < this._lengths.length; ++i) {
+            var partLen = this._lengths[i];
+            if (len >= sum && len <= sum + partLen) {
+                return Formula.bezier(this._partions[i], 2, (len - sum) / partLen);
+            }
+            sum += partLen;
+        }
+        return null;
+    };
+    /* 获取分段个数 */
+    curve.getPartionCount = function() {
+        return this._partions.length;
+    };
+    /* 绘制坐标点 */
+    curve.drawPoint = function(ctx, offset, style, radius) {
+        if (undefined === ctx || null === ctx || 0 === this._points.length) {
+            return;
+        }
+        var ox = 0, oy = 0;
+        if (offset instanceof Array && 'number' === typeof(offset[0]) && 'number' === typeof(offset[1])) {
+            ox = offset[0];
+            oy = offset[1];
+        }
+        if ('number' !== typeof(radius) || radius <= 0) {
+            radius = 1;
+        }
+        if ('string' !== typeof(style)) {
+            style = "#000000";
+        }
+        ctx.fillStyle = style;
+        for (var i = 0; i < this._points.length; ++i) {
+            var pt = this._points[i];
+            ctx.beginPath();
+            ctx.arc(pt[0] + ox, pt[1] + oy, radius, 0, 360, true);
+            ctx.fill();
+        }
+    };
+    /* 绘制路径 */
+    curve.drawPath = function(ctx, offset) {
+        if (undefined === ctx || null === ctx || 0 === this._partions.length) {
+            return false;
+        }
+        var ox = 0, oy = 0;
+        if (offset instanceof Array && 'number' === typeof(offset[0]) && 'number' === typeof(offset[1])) {
+            ox = offset[0];
+            oy = offset[1];
+        }
+        ctx.beginPath();
+        for (var i = 0; i < this._partions.length; ++i) {
+            var partion = this._partions[i];
+            if (0 === i) {
+                ctx.moveTo(partion[0][0] + ox, partion[0][1] + oy);
+            } else {
+                ctx.lineTo(partion[0][0] + ox, partion[0][1] + oy);
+            }
+            if (3 === partion.length) {
+                ctx.quadraticCurveTo(partion[1][0] + ox, partion[1][1] + oy, partion[2][0] + ox, partion[2][1] + oy);
+            } else if (4 === partion.length) {
+                ctx.bezierCurveTo(partion[1][0] + ox, partion[1][1] + oy, partion[2][0] + ox, partion[2][1] + oy, partion[3][0] + ox, partion[3][1] + oy);
+            }
+        }
+        return true;
+    };
+    if (points instanceof Array) {
+        curve.update(points, ratio);
+    }
+    return curve;
+}
+//----------------------------------------------------------------------
