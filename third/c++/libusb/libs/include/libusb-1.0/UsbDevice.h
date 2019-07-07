7f031ae7eed8abe66ede4377bf5b57f0425f3265
jaronho <892134825@qq.com>
2019-07-07 19:03:09 +0800

    third/c++/libusb/libs/include/libusb-1.0

--- third/c++/libusb/libs/include/libusb-1.0/UsbDevice.h
+++ third/c++/libusb/libs/include/libusb-1.0/UsbDevice.h
@@ -17,6 +17,48 @@ class UsbDevice {
 public:
     UsbDevice(void) : classCode(0), vendorId(0), productId(0), busNumber(0), portNumber(0), address(0) {}
 
+    std::string className(void) {
+        switch (classCode) {
+        case LIBUSB_CLASS_PER_INTERFACE:
+            return "CLASS_PER_INTERFACE";
+        case LIBUSB_CLASS_AUDIO:
+            return "Audio class";
+        case LIBUSB_CLASS_COMM:
+            return "Communications class";
+        case LIBUSB_CLASS_HID:
+            return "Human Interface Device class";
+        case LIBUSB_CLASS_PHYSICAL:
+            return "Physical";
+        case LIBUSB_CLASS_PRINTER:
+            return "Printer class";
+        case LIBUSB_CLASS_IMAGE:
+            return "Image class";
+        case LIBUSB_CLASS_MASS_STORAGE:
+            return "Mass storage class";
+        case LIBUSB_CLASS_HUB:
+            return "Hub class";
+        case LIBUSB_CLASS_DATA:
+            return "Data class";
+        case LIBUSB_CLASS_SMART_CARD:
+            return "Smart Card";
+        case LIBUSB_CLASS_CONTENT_SECURITY:
+            return "Content Security";
+        case LIBUSB_CLASS_VIDEO:
+            return "Video";
+        case LIBUSB_CLASS_PERSONAL_HEALTHCARE:
+            return "Personal Healthcare";
+        case LIBUSB_CLASS_DIAGNOSTIC_DEVICE:
+            return "Diagnostic Device";
+        case LIBUSB_CLASS_WIRELESS:
+            return "Wireless class";
+        case LIBUSB_CLASS_APPLICATION:
+            return "Application class";
+        case LIBUSB_CLASS_VENDOR_SPEC:
+            return "Class is vendor-specific";
+        }
+        return std::to_string(classCode);
+    }
+
 public:
     int classCode;                  /* class code for the device, see libusb_class_code */
     int vendorId;                   /* the device vendor id */
