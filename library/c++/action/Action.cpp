7c26b99416b6784254cc8fcf052ad94532be0ae6
jaronho <892134825@qq.com>
2019-07-20 19:44:47 +0800

    library/c++/action

--- library/c++/action/Action.cpp
+++ library/c++/action/Action.cpp
@@ -6,22 +6,39 @@
 #include "Action.h"
 #include <unistd.h>
 #include <list>
+#include <map>
 #include <mutex>
 #include <thread>
 
 static std::mutex sAsyncProcessMutex;
 static std::list<Action*> sAsyncProcessList;
-static std::mutex sAsyncFinishMutex;
-static std::list<Action*> sAsyncFinishList;
+static std::mutex sDelayProcessMutex;
+static std::map<unsigned long long, Action*> sDelayProcessMap;
+static std::mutex sFinishMutex;
+static std::list<Action*> sFinishList;
 
-void Action::aloneHandler(Action* act) {
+static unsigned long long generateActionId(void) {
+    static int sY = 0, sM = 0, sD = 0, sH = 0, sMM = 0, sS = 0, sIdx = 1;
+    time_t t = time(nullptr);
+    struct tm* dt = localtime(&t);
+    int year = 1900 + dt->tm_year, mon = 1 + dt->tm_mon, mday = dt->tm_mday, hour = dt->tm_hour, min = dt->tm_min, sec = dt->tm_sec;
+    if (year == sY && mon == sM && mday == sD && hour == sH && min == sMM && sec == sS) {
+        sIdx++;
+    } else {
+        sY = year; sM = mon; sD = mday; sH = hour; sMM = min; sS = sec; sIdx = 1;
+    }
+    return (unsigned long long)sY * 10000000000000 + (unsigned long long)sM * 100000000000 + (unsigned long long)sD * 1000000000 +
+           (unsigned long long)sH * 10000000 + (unsigned long long)sMM * 100000 + (unsigned long long)sS * 1000 + (unsigned long long)sIdx;
+}
+
+void Action::asyncAloneHandler(Action* act) {
     if (act) {
         act->onProcess();
         if (act->mFinishCallback) {
             if (act->mAsyncCallbackToMainThread) {
-                sAsyncFinishMutex.lock();
-                sAsyncFinishList.push_back(act);
-                sAsyncFinishMutex.unlock();
+                sFinishMutex.lock();
+                sFinishList.push_back(act);
+                sFinishMutex.unlock();
             } else {
                 act->mFinishCallback(act);
                 delete act;
@@ -34,7 +51,7 @@ void Action::aloneHandler(Action* act) {
     }
 }
 
-void Action::groupHandler(void) {
+void Action::asyncGroupHandler(void) {
     while (true) {
         Action* act = nullptr;
         sAsyncProcessMutex.lock();
@@ -47,9 +64,9 @@ void Action::groupHandler(void) {
             act->onProcess();
             if (act->mFinishCallback) {
                 if (act->mAsyncCallbackToMainThread) {
-                    sAsyncFinishMutex.lock();
-                    sAsyncFinishList.push_back(act);
-                    sAsyncFinishMutex.unlock();
+                    sFinishMutex.lock();
+                    sFinishList.push_back(act);
+                    sFinishMutex.unlock();
                 } else {
                     act->mFinishCallback(act);
                     delete act;
@@ -64,23 +81,62 @@ void Action::groupHandler(void) {
     }
 }
 
+void Action::delayGroupHandler(void) {
+    const int STEP = 10000; /* 10毫秒 */
+    while (true) {
+        std::list<Action*> finishList;
+        std::list<Action*>::iterator finishIter;
+        std::map<unsigned long long, Action*>::iterator iter;
+        sDelayProcessMutex.lock();
+        iter = sDelayProcessMap.begin();
+        for (; sDelayProcessMap.end() != iter; ++iter) {
+            iter->second->mDelayTime -= STEP;
+            if (iter->second->mDelayTime <= 0) {
+                iter->second->mDelayTime = 0;
+                finishList.push_back(iter->second);
+            }
+        }
+        finishIter = finishList.begin();
+        for (; finishList.end() != finishIter; ++finishIter) {
+            iter = sDelayProcessMap.find((*finishIter)->getId());
+            if (sDelayProcessMap.end() != iter) {
+                sDelayProcessMap.erase(iter);
+            }
+        }
+        sDelayProcessMutex.unlock();
+        sFinishMutex.lock();
+        finishIter = finishList.begin();
+        for (; finishList.end() != finishIter; ++finishIter) {
+            sFinishList.push_back(*finishIter);
+        }
+        sFinishMutex.unlock();
+        usleep(STEP);
+    }
+}
+
 Action::Action(ACTION_FINISH_CALLBACK callback, RunType type) {
+    mId = generateActionId();
     mFinishCallback = callback;
-    mRunType = type;
+    mAllowRunType = type;
 }
 
 Action::~Action(void) {}
+
+unsigned long long Action::getId(void) {
+    return mId;
+}
 
 int Action::syncRun(Action* act, bool autoDestroy) {
     if (!act) {
         return 1;
     }
-    if (RT_NONE == act->mRunType) {
+    if (RT_NONE == act->mAllowRunType) {
         return 2;
     }
-    if (RT_SYNC != act->mRunType && RT_BOTH != act->mRunType) {
+    if (RT_SYNC != act->mAllowRunType && RT_BOTH != act->mAllowRunType) {
         return 3;
     }
+    act->mNowRunType = RT_SYNC;
     act->onProcess();
     if (act->mFinishCallback) {
         act->mFinishCallback(act);
@@ -96,22 +152,23 @@ int Action::asyncRun(Action* act, bool alone, bool callbackToMainThread) {
     if (!act) {
         return 1;
     }
-    if (RT_NONE == act->mRunType) {
+    if (RT_NONE == act->mAllowRunType) {
         return 2;
     }
-    if (RT_ASYNC != act->mRunType && RT_BOTH != act->mRunType) {
+    if (RT_ASYNC != act->mAllowRunType && RT_BOTH != act->mAllowRunType) {
         return 3;
     }
+    act->mNowRunType = RT_ASYNC;
     act->mAsyncCallbackToMainThread = callbackToMainThread;
     if (alone) {
-        std::thread aloneThread(aloneHandler, act);
-        aloneThread.detach();
+        std::thread asyncAloneThread(asyncAloneHandler, act);
+        asyncAloneThread.detach();
     } else {
-        static bool sInitGroupThread = true;
-        if (sInitGroupThread) {
-            sInitGroupThread = false;
-            std::thread groupThread(groupHandler);
-            groupThread.detach();
+        static bool sInitAsyncGroupThread = true;
+        if (sInitAsyncGroupThread) {
+            sInitAsyncGroupThread = false;
+            std::thread asyncGroupThread(asyncGroupHandler);
+            asyncGroupThread.detach();
         }
         sAsyncProcessMutex.lock();
         sAsyncProcessList.push_back(act);
@@ -120,15 +177,42 @@ int Action::asyncRun(Action* act, bool alone, bool callbackToMainThread) {
     return 0;
 }
 
-void Action::asyncListen(void) {
-    Action* act = nullptr;
-    sAsyncFinishMutex.lock();
-    if (sAsyncFinishList.size() > 0) {
-        act = *(sAsyncFinishList.begin());
-        sAsyncFinishList.pop_front();
+int Action::delayRun(Action* act, unsigned long msec) {
+    if (!act) {
+        return 1;
     }
-    sAsyncFinishMutex.unlock();
+    if (RT_NONE == act->mAllowRunType) {
+        return 2;
+    }
+    if (RT_DELAY != act->mAllowRunType && RT_BOTH != act->mAllowRunType) {
+        return 3;
+    }
+    act->mNowRunType = RT_DELAY;
+    act->mDelayTime = msec;
+    static bool sInitDelayGroupThread = true;
+    if (sInitDelayGroupThread) {
+        sInitDelayGroupThread = false;
+        std::thread delayGroupThread(delayGroupHandler);
+        delayGroupThread.detach();
+    }
+    sDelayProcessMutex.lock();
+    sDelayProcessMap[act->getId()] = act;
+    sDelayProcessMutex.unlock();
+    return 0;
+}
+
+void Action::listen(void) {
+    Action* act = nullptr;
+    sFinishMutex.lock();
+    if (sFinishList.size() > 0) {
+        act = *(sFinishList.begin());
+        sFinishList.pop_front();
+    }
+    sFinishMutex.unlock();
     if (act) {
+        if (RT_DELAY == act->mNowRunType) {
+            act->onProcess();
+        }
         if (act->mFinishCallback) {
             act->mFinishCallback(act);
         }
--- library/c++/action/Action.h
+++ library/c++/action/Action.h
@@ -15,16 +15,29 @@ class Action {
 public:
     /* 执行方式 */
     enum RunType {
-        RT_NONE,                /* 不允许使用syncRun和asyncRun执行 */
+        RT_NONE,                /* 不允许使用syncRun,asyncRun,delayRun执行 */
         RT_SYNC,                /* 只能同步执行 */
         RT_ASYNC,               /* 只能异步执行 */
-        RT_BOTH                 /* 允许同步,异步执行 */
+        RT_DELAY,               /* 只能延迟执行 */
+        RT_BOTH                 /* 允许同步,异步,延迟执行 */
     };
 public:
     Action(ACTION_FINISH_CALLBACK callback = nullptr, RunType type = RT_BOTH);
     virtual ~Action(void);
 
+    /*
+     * Brief:	获取动作id
+     * Param:	void
+     * Return:	unsigned long long
+     */
+    unsigned long long getId(void);
+
 protected:
+    /*
+     * Brief:	动作处理函数,需要子线程实现(同步:在动作调用线程执行,异步:在子线程排队执行,延迟:在主线程执行)
+     * Param:	void
+     * Return:	void
+     */
     virtual void onProcess(void) = 0;
 
 public:
@@ -34,7 +47,7 @@ public:
      *          autoDestroy - true.动作自动销毁,false.动作需要手动销毁
      * Return:	0.成功
      *          1.act为空指针
-     *          2.act动作不允许使用syncRun和asyncRun执行
+     *          2.act动作不允许使用syncRun,asyncRun,delayRun执行
      *          3.act动作不允许同步执行
      */
     static int syncRun(Action* act, bool autoDestroy = false);
@@ -46,27 +59,43 @@ public:
      *          callbackToMainThread - true.动作结束回调在主线程执行,false.动作回调在子线程执行
      * Return:	0.成功
      *          1.act为空指针
-     *          2.act动作不允许使用syncRun和asyncRun执行
+     *          2.act动作不允许使用syncRun,asyncRun,delayRun执行
      *          3.act动作不允许异步执行
      */
     static int asyncRun(Action* act, bool alone = false, bool callbackToMainThread = true);
 
     /*
-     * Brief:	异步监听(需要在主线程中循环调用),若动作有回调函数,则回调函数将在主线程中被调用
+     * Brief:	延迟执行(在主线程中执行,动作自动销毁)
+     * Param:	act - 动作
+     *          msec - 延迟时间(毫秒)
+     * Return:	0.成功
+     *          1.act为空指针
+     *          2.act动作不允许使用syncRun,asyncRun,delayRun执行
+     *          3.act动作不允许延迟执行
+     */
+    static int delayRun(Action* act, unsigned long msec);
+
+    /*
+     * Brief:	监听回调(需要在主线程中循环调用),若动作有回调函数,则回调函数将在主线程中被调用
      * Param:	void
      * Return:	void
      */
-    static void asyncListen(void);
+    static void listen(void);
 
 private:
-    static void aloneHandler(Action* act);
+    static void asyncAloneHandler(Action* act);
 
-    static void groupHandler(void);
+    static void asyncGroupHandler(void);
+
+    static void delayGroupHandler(void);
 
 private:
+    unsigned long long mId;                             /* id */
     ACTION_FINISH_CALLBACK mFinishCallback;             /* 动作结束回调函数 */
-    RunType mRunType;                                   /* 动作执行方式 */
+    RunType mAllowRunType;                              /* 动作允许的执行方式 */
+    RunType mNowRunType;                                /* 动作当前执行方式 */
     bool mAsyncCallbackToMainThread;                    /* 异步执行:是否在主线程执行回调 */
+    unsigned long mDelayTime;                           /* 延迟执行:时间(毫秒) */
 };
 
 #endif	/* _ACTION_H_ */
