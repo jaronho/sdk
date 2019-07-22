88a4233758fd669e4d3da9284c0bdc6fc7c74873
jaronho <892134825@qq.com>
2019-07-22 11:57:53 +0800

    library/c++/__MACROS__.h

--- library/c++/__MACROS__.h
+++ library/c++/__MACROS__.h
@@ -3,10 +3,10 @@
  * Date:    2019-07-22
  * Brief:	全局宏定义
  **********************************************************************/
-#ifndef _MACROS_H_
-#define _MACROS_H_
+#ifndef __MACROS_H__
+#define __MACROS_H__
 
 #define G_VAR_NAME(var)     (#var)              /* 变量名 */
 #define G_NAME_VALUE(var)   {#var, var}         /* 键值对:{变量名,变量值} */
 
-#endif	/* _MACROS_H_ */
+#endif	/* __MACROS_H__ */
