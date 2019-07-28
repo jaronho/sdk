ff23f2af616b6c97eba487cef951f2ca91e36241
jaronho <892134825@qq.com>
2019-07-28 20:49:53 +0800

    utils/python/ftp/ftp_bridge_clear.py

--- utils/python/ftp/ftp_bridge_clear.py
+++ utils/python/ftp/ftp_bridge_clear.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python2
 # -*- coding: UTF-8 -*-
-import argparse
 import os
-import sys
 
 """ 主入口函数 """
 def main():
-    if 1 == len(sys.argv):
-        sys.argv.append("-h")
-    parser = argparse.ArgumentParser(description="用于清空FTP桥接策略",
-                                     usage=os.path.basename(__file__) + " [-h]")
-    parser.parse_args()
-    args = vars(parser.parse_args())
     os.popen("iptables -t nat -F")
 
 if "__main__" == __name__:
