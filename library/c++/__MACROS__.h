785fc6acacf40839d6144fc997dcbed2b220b83a
jaronho <892134825@qq.com>
2020-04-28 16:36:48 +0800

    增加符号拼接宏定义接口

--- library/c++/__MACROS__.h
+++ library/c++/__MACROS__.h
@@ -6,7 +6,8 @@
 #ifndef __MACROS_H__
 #define __MACROS_H__
 
-#define G_VAR_NAME(var)     (#var)              /* 变量名 */
-#define G_NAME_VALUE(var)   {#var, var}         /* 键值对:{变量名,变量值} */
+#define G_JOIN(a, b)              a##b                          /* 拼接符号a和b为ab */
+#define G_STR(a)                  (#a)                          /* 获取符号a的字符串名 */
+#define G_NAME_VALUE(var)         {#var, var}                   /* 生成键值对:{变量名,变量值} */
 
 #endif	/* __MACROS_H__ */
