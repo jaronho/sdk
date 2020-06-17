551de65807d6a956200392ac71f8d35ff4eb2144
jaronho <651497857@qq.com>
2020-06-17 14:56:17 +0800

    Fixed:优化md5模块接口

--- third/c++/md5/MD5.h
+++ third/c++/md5/MD5.h
@@ -1,18 +1,18 @@
-#ifndef _MD5_H__
-#define _MD5_H__
-/* MD5.H - header file for MD5C.C */
+#ifndef _MD5_H_
+#define _MD5_H_
+/* md5.h - header file for md5.c */
 
 /* Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
    rights reserved.
 
    License to copy and use this software is granted provided that it
-   is identified as the "RSA Data Security, Inc. MD5 Message-Digest
+   is identified as the "RSA Data Security, Inc. md5 Message-Digest
    Algorithm" in all material mentioning or referencing this software
    or this function.
 
    License is also granted to make and use derivative works provided
    that such works are identified as "derived from the RSA Data
-   Security, Inc. MD5 Message-Digest Algorithm" in all material
+   Security, Inc. md5 Message-Digest Algorithm" in all material
    mentioning or referencing the derived work.
 
    RSA Data Security, Inc. makes no representations concerning either
@@ -26,18 +26,26 @@
    2007-09-15 Last modified by cheungmine.
  */
 
-/* MD5 context. */
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* md5 context. */
 typedef struct {
-    unsigned int state[4];                /* state (ABCD) */
-    unsigned int count[2];                /* number of bits, modulo 2^64 (lsb first) */
-    unsigned char buffer[64];    /* input buffer */
-} MD5_CTX;
+    unsigned int state[4];                  /* state (ABCD) */
+    unsigned int count[2];                  /* number of bits, modulo 2^64 (lsb first) */
+    unsigned char buffer[64];               /* input buffer */
+} md5_ctx_t;
 
-void  MD5_init (MD5_CTX *);
-void  MD5_update (MD5_CTX *, const unsigned char *str, unsigned int len);
-void  MD5_fini (unsigned char[16], MD5_CTX *);
+extern void md5_init(md5_ctx_t* context);
+extern void md5_update(md5_ctx_t* context, const unsigned char* input, unsigned int inputLen);
+extern void md5_fini(md5_ctx_t* context, unsigned char digest[16]);
 
-char* MD5_sign (const unsigned char *str, unsigned int len);
+extern const char* md5_sign(const unsigned char* input, unsigned int inputLen);
 
-#endif    /* _MD5_H__ */
+#ifdef __cplusplus
+}
+#endif
 
+#endif  /* _MD5_H_ */
