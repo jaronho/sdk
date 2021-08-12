535e96fc2fbf7eb597033883e8235d94cca62afa
jaronho <892134825@qq.com>
2021-08-12 17:25:47 +0800

    feat: 增加超时检测宏

--- base/cxx/timewatch/timewatch/time_watcher.h
+++ base/cxx/timewatch/timewatch/time_watcher.h
@@ -67,6 +67,23 @@ public:
         }
     }
 
+    /**
+	 * @brief 检测是否超时
+	 * @param timeout 超时(毫秒)
+	 * @return true-超时, false-不超时
+	 */
+    bool check(long long timeout)
+    {
+        std::chrono::steady_clock::time_point prevWatch = m_watch;
+        m_watch = std::chrono::steady_clock::now();
+        std::chrono::milliseconds elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(m_watch - prevWatch);
+        if (elapsed.count() >= timeout)
+        {
+            return true;
+        }
+        return false;
+    }
+
 private:
     std::chrono::steady_clock::time_point m_begin; /* 开始时间 */
     std::chrono::steady_clock::time_point m_watch; /* 观察时间 */
@@ -85,6 +102,9 @@ private:
 
 /* 时间观察者观察(非线程安全) */
 #define TIME_WATCHER_WATCH(watcher, subTag) watcher.watch(subTag);
+
+/* 时间观察者观察(非线程安全) */
+#define TIME_WATCHER_CHECK(watcher, timeout) watcher.check(timeout);
 
 /* 启动简单时间观察者(非线程安全,自动命名变量) */
 #define START_SIMPLE_TIME_WATCHER(endFunc) MAKE_TIME_WATCHER(TIME_WATCHER_VAR(_timeWatcher_), nullptr, endFunc, "")
