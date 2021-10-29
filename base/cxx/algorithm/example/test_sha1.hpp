7e8a8866b276b3e11411b0027edbd5f5c1ad31f6
jaronho <892134825@qq.com>
2021-10-29 10:48:49 +0800

    feat: 修改算法示例

--- base/cxx/algorithm/example/test_sha1.hpp
+++ base/cxx/algorithm/example/test_sha1.hpp
@@ -10,7 +10,7 @@ void testSha1()
     printf("\n============================== test sha1 =============================\n");
     std::string str = "0123456789abcdefg";
     printf("str: %s\n", str.c_str());
-    char* output = sha1Sign((const unsigned char*)str.c_str(), str.size());
+    char* output = sha1SignStr((const unsigned char*)str.c_str(), str.size());
     if (output)
     {
         printf("sha1: %s\n", output);
