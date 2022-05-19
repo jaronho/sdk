84a6ecf3d79f689f33b7adbd813b67029638721b
jaronho <892134825@qq.com>
2022-05-19 19:58:20 +0800

    feat: 添加定时器注释

--- base/cxx/threading/threading/diagnose/diagnose.h
+++ base/cxx/threading/threading/diagnose/diagnose.h
@@ -143,13 +143,32 @@ public:
     static void setTimerTriggerExceptionStateCallback(const TimerTriggerExceptionStateCallback& timerTriggerStateCb);
 
     /**
-     * @brief 获取任务诊断信息
+     * @brief 获取任务诊断信息, 格式例如:
+        [ // 执行者(线程池)列表
+            {
+                "name":"workers", // 执行者(线程池)名称
+                "count":16, // 线程池中所有未结束的任务数量
+                "task": // 任务列表: 任务ID/名称, 所在线程ID/名称, 状态(created,queuing,running,finished), 各状态耗时(单位: ns,us,ms)
+                [
+                    // 任务信息: 任务ID/名称, 所在线程ID/名称, 状态(created,queuing,running,finished), 各状态耗时(单位: ns,us,ms)
+                    {"id":6770523678363649,"name":"task_3","thread_id":19288,"thread_name":"workers-2","state":"running","queue":"71 us","run":"4004 ms"},
+                    {"id":6770523674263553,"name":"task_1","thread_id":24100,"thread_name":"workers-1","state":"running","queue":"67 us","run":"5005 ms"},
+                    {"id":6770523688615937,"name":"task_8","thread_id":0,"thread_name":"","state":"queuing","queue":"1502 ms"},
+                    {"id":6770523686563841,"name":"task_7","thread_id":0,"thread_name":"","state":"queuing","queue":"2002 ms"}
+                ]
+            }
+        ]
      * @return 诊断信息字符串(JSON)
      */
     static std::string getTaskDiagnoseInfo();
 
     /**
-     * @brief 获取定时器诊断信息
+     * @brief 获取定时器诊断信息, 格式例如:
+        [ // 定时器触发列表
+            // 触发信息: 定时器ID/名称, 状态(triggered,queuing,running,finished), 各状态耗时(单位: ns,us,ms)
+            {"id":6770523674243072,"name":"timer_1","state":"running","queue":"516 ms","run":"5 ms"},
+            {"id":6770523674245234,"name":"timer_2","state":"running","queue":"900 ms","run":"1002 ms"}
+        ]
      * @return 诊断信息字符串(JSON)
      */
     static std::string getTimerDiagnoseInfo();
