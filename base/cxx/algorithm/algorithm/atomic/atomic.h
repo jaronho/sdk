63d4c9625fa5e75c7fa9df33508f54e0e8f4e1e2
jaronho <892134825@qq.com>
2023-02-14 14:25:05 +0800

    feat: 修改原子类型

--- base/cxx/algorithm/algorithm/atomic/atomic.h
+++ base/cxx/algorithm/algorithm/atomic/atomic.h
@@ -8,7 +8,7 @@ namespace algorithm
  * @brief 原子类型(线程安全)
  */
 template<typename T>
-#define STYPE_CMP_FUNC std::function<int(const T& a, const T& b)> /* 值相等比较函数, 返回值: 负数-小于, 0-等于, 正数-大于 */
+#define ATOMIC_CMP_FUNC std::function<int(const T& a, const T& b)> /* 值比较函数, 返回值: 负数-小于, 0-等于, 正数-大于 */
 class Atomic
 {
 public:
@@ -19,13 +19,13 @@ public:
      * @param value 值
      * @param cmpFunc 比较函数, 参数: a-当前值, b-比较值, 返回: 负数-小于, 0-等于, 正数-大于
      */
-    Atomic(const T& value, const STYPE_CMP_FUNC& cmpFunc = nullptr) : m_value(value), m_cmpFunc(cmpFunc) {}
+    Atomic(const T& value, const ATOMIC_CMP_FUNC& cmpFunc = nullptr) : m_value(value), m_cmpFunc(cmpFunc) {}
 
     /**
      * @brief 设置值比较函数
      * @param cmpFunc 比较函数, 参数: a-当前值, b-比较值, 返回: 负数-小于, 0-等于, 正数-大于
      */
-    void setCmpFunc(const STYPE_CMP_FUNC& cmpFunc)
+    void setCmpFunc(const ATOMIC_CMP_FUNC& cmpFunc)
     {
         std::lock_guard<std::mutex> locker(m_mutex);
         m_cmpFunc = cmpFunc;
@@ -60,7 +60,7 @@ public:
     bool operator==(const T& value)
     {
         T tmpValue = T{};
-        STYPE_CMP_FUNC compFunc = nullptr;
+        ATOMIC_CMP_FUNC compFunc = nullptr;
         {
             std::lock_guard<std::mutex> locker(m_mutex);
             tmpValue = m_value;
@@ -76,7 +76,7 @@ public:
     bool operator!=(const T& value)
     {
         T tmpValue = T{};
-        STYPE_CMP_FUNC compFunc = nullptr;
+        ATOMIC_CMP_FUNC compFunc = nullptr;
         {
             std::lock_guard<std::mutex> locker(m_mutex);
             tmpValue = m_value;
@@ -92,7 +92,7 @@ public:
     bool operator>(const T& value)
     {
         T tmpValue = T{};
-        STYPE_CMP_FUNC compFunc = nullptr;
+        ATOMIC_CMP_FUNC compFunc = nullptr;
         {
             std::lock_guard<std::mutex> locker(m_mutex);
             tmpValue = m_value;
@@ -108,7 +108,7 @@ public:
     bool operator<(const T& value)
     {
         T tmpValue = T{};
-        STYPE_CMP_FUNC compFunc = nullptr;
+        ATOMIC_CMP_FUNC compFunc = nullptr;
         {
             std::lock_guard<std::mutex> locker(m_mutex);
             tmpValue = m_value;
@@ -124,7 +124,7 @@ public:
     bool operator>=(const T& value)
     {
         T tmpValue = T{};
-        STYPE_CMP_FUNC compFunc = nullptr;
+        ATOMIC_CMP_FUNC compFunc = nullptr;
         {
             std::lock_guard<std::mutex> locker(m_mutex);
             tmpValue = m_value;
@@ -140,7 +140,7 @@ public:
     bool operator<=(const T& value)
     {
         T tmpValue = T{};
-        STYPE_CMP_FUNC compFunc = nullptr;
+        ATOMIC_CMP_FUNC compFunc = nullptr;
         {
             std::lock_guard<std::mutex> locker(m_mutex);
             tmpValue = m_value;
@@ -156,6 +156,6 @@ public:
 private:
     std::mutex m_mutex;
     T m_value = T{}; /* 值 */
-    STYPE_CMP_FUNC m_cmpFunc = nullptr; /* 值比较函数 */
+    ATOMIC_CMP_FUNC m_cmpFunc = nullptr; /* 值比较函数 */
 };
 } // namespace algorithm
