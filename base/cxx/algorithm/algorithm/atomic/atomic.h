8fb1b8369784d934bc3eeff78b0f4f750827d937
jaronho <892134825@qq.com>
2023-12-27 22:37:27 +0800

    feat: [算法]优化原子类型回调

--- base/cxx/algorithm/algorithm/atomic/atomic.h
+++ base/cxx/algorithm/algorithm/atomic/atomic.h
@@ -6,11 +6,14 @@ namespace algorithm
 {
 /**
  * @brief 原子类型(线程安全), T如果是类/结构体对象类型则需要实现运算符重载: 
- *        bool operator==(const T& other) const; bool operator>(const T& other) const; bool operator<(const T& other) const;
+ *        bool operator==(const T& other) const;
+ *        bool operator>(const T& other) const;
+ *        bool operator<(const T& other) const;
  */
 template<typename T>
 #define ATOMIC_CMP_FUNC std::function<int(const T& a, const T& b)> /* 值比较函数, 返回值: 负数-小于, 0-等于, 正数-大于 */
-#define ATOMIC_UPDATE_CALLBACK std::function<void(const T& value, bool changed)> /* 值更新回调, 参数: value-最新值, changed-是否变化 */
+#define ATOMIC_EVA_CALLBACK \
+    std::function<void(const T& oldVal, const T& newVal, bool changed)> /* 值赋值回调, 参数: oldVal-旧值, newVal-新值, changed-是否变化 */
 class Atomic
 {
 public:
@@ -20,8 +23,12 @@ public:
      * @brief 构造函数
      * @param value 值
      * @param cmpFunc 值比较函数, 参数: a-当前值, b-比较值, 返回: 负数-小于, 0-等于, 正数-大于
+     * @param evaCb 值赋值回调, 参数: oldVal-旧值, newVal-新值, changed-是否变化
      */
-    Atomic(const T& value, const ATOMIC_CMP_FUNC& cmpFunc = nullptr) : m_value(value), m_cmpFunc(cmpFunc) {}
+    Atomic(const T& value, const ATOMIC_CMP_FUNC& cmpFunc = nullptr, const ATOMIC_EVA_CALLBACK& evaCb = nullptr)
+        : m_value(value), m_cmpFunc(cmpFunc), m_evaCallback(evaCb)
+    {
+    }
 
     /**
      * @brief 设置值比较函数
@@ -34,13 +41,13 @@ public:
     }
 
     /**
-     * @brief 设置值更新回调
-     * @param callback 值更新回调, 参数: value-最新值, changed-是否变化
+     * @brief 设置值赋值回调
+     * @param callback 值赋值回调, 参数: oldVal-旧值, newVal-新值, changed-是否变化
      */
-    void setUpdateCallback(const ATOMIC_UPDATE_CALLBACK& callback)
+    void setEvaCallback(const ATOMIC_EVA_CALLBACK& callback)
     {
         std::lock_guard<std::mutex> locker(m_mutex);
-        m_updateCallback = callback;
+        m_evaCallback = callback;
     }
 
     /**
@@ -60,17 +67,18 @@ public:
      */
     bool set(const T& value)
     {
-        bool changed = false;
-        ATOMIC_UPDATE_CALLBACK updateCallback = nullptr;
+        T oldVal;
+        ATOMIC_EVA_CALLBACK evaCallback = nullptr;
         {
             std::lock_guard<std::mutex> locker(m_mutex);
-            changed = (value != m_value);
+            oldVal = m_value;
             m_value = value;
-            updateCallback = m_updateCallback;
+            evaCallback = m_evaCallback;
         }
-        if (updateCallback)
+        auto changed = (value != oldVal);
+        if (evaCallback)
         {
-            updateCallback(value, changed);
+            evaCallback(oldVal, value, changed);
         }
         return changed;
     }
@@ -181,6 +189,6 @@ private:
     std::mutex m_mutex;
     T m_value = T{}; /* 值 */
     ATOMIC_CMP_FUNC m_cmpFunc = nullptr; /* 值比较函数 */
-    ATOMIC_UPDATE_CALLBACK m_updateCallback = nullptr; /* 值更新回调 */
+    ATOMIC_EVA_CALLBACK m_evaCallback = nullptr; /* 值赋值回调 */
 };
 } // namespace algorithm
