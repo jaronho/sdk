e5ed590b1ea716bfd47ffb4ca69bfcb775d5c97f
jaronho <892134825@qq.com>
2024-04-25 10:34:33 +0800

    feat: [公共库]优化目录MD5计算接口参数

--- comlib/cxx/toolkit/toolkit/tool.cpp
+++ comlib/cxx/toolkit/toolkit/tool.cpp
@@ -10,24 +10,26 @@
 namespace toolkit
 {
 std::string Tool::md5Directory(const std::string& path,
-                               const std::function<void(size_t nowFolderCount, size_t nowFileCount, size_t nowFileSize)>& progressCb)
+                               const std::function<void(const std::string& name, bool isDir, size_t fileSize)>& progressCb)
 {
     std::string value;
-    size_t nowFolderCount = 0, nowFileCount = 0, nowFileSize = 0;
     utility::PathInfo pi(path);
     pi.traverse(
         [&](const std::string& name, const utility::FileAttribute& attr, int depth) {
+            if (progressCb)
+            {
+                progressCb(name, attr.isDir, attr.size);
+            }
             auto relativeName = utility::StrTool::replace(name.substr(pi.path().size()), "\\\\", "/");
             value += relativeName;
             value = algorithm::md5SignStr((const unsigned char*)value.c_str(), value.size());
-            ++nowFolderCount;
-            if (progressCb)
-            {
-                progressCb(nowFolderCount, nowFileCount, nowFileSize);
-            }
             return true;
         },
         [&](const std::string& name, const utility::FileAttribute& attr, int depth) {
+            if (progressCb)
+            {
+                progressCb(name, attr.isDir, attr.size);
+            }
             auto relativeName = utility::StrTool::replace(name.substr(pi.path().size()), "\\\\", "/");
             value += relativeName;
             auto buf = algorithm::md5SignFile(name.c_str(), 1024 * 1024);
@@ -37,12 +39,6 @@ std::string Tool::md5Directory(const std::string& path,
                 free(buf);
             }
             value = algorithm::md5SignStr((const unsigned char*)value.c_str(), value.size());
-            ++nowFileCount;
-            nowFileSize += attr.size;
-            if (progressCb)
-            {
-                progressCb(nowFolderCount, nowFileCount, nowFileSize);
-            }
         },
         nullptr, true, false);
     return value;
--- comlib/cxx/toolkit/toolkit/tool.h
+++ comlib/cxx/toolkit/toolkit/tool.h
@@ -13,11 +13,10 @@ public:
     /**
      * @brief 计算目录MD5值
      * @param path 目录
-     * @param progressCb 进度回调, 参数: nowFolderCount-已计算的文件夹数, nowFileCount-已计算的文件数, nowFileSize-已计算的文件总大小(字节)
+     * @param progressCb 进度回调, 参数: name-文件路径, isDir-是否目录, fileSize-文件大小(字节, 目录恒为4个字节)
      * @return 目录MD5值
      */
-    static std::string
-    md5Directory(const std::string& path,
-                 const std::function<void(size_t nowFolderCount, size_t nowFileCount, size_t nowFileSize)>& progressCb = nullptr);
+    static std::string md5Directory(const std::string& path,
+                                    const std::function<void(const std::string& name, bool isDir, size_t fileSize)>& progressCb = nullptr);
 };
 } // namespace toolkit
