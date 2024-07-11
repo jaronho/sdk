74b59ed8660619c1055865cff76e53d1f44e32d6
jaronho <892134825@qq.com>
2024-07-11 15:31:59 +0800

    feat: [USB]USB设备查看工具增加监听模式

--- base/cxx/usb/example/usb_watch.cpp
+++ base/cxx/usb/example/usb_watch.cpp
@@ -1,42 +1,51 @@
-﻿#include <stdio.h>
+#include <algorithm>
+#include <chrono>
+#include <stdio.h>
 #include <string.h>
+#include <sys/timeb.h>
+#include <thread>
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif
 
 #include "../usb/usb.h"
 
-/**
- * USB设备查看工具
- * 用法: ./usb_watch [选项]
- * 选项: -h 帮助信息
- *       -t 是否树形显示
- * 例如: ./usb_watch -t
- */
-int main(int argc, char** argv)
+static std::vector<std::shared_ptr<usb::Usb>> s_usbList; /* USB设备列表缓存 */
+
+std::string getDateTime()
 {
-    /* 参数值 */
-    bool treeFlag = false;
-    /* 解析参数 */
-    for (int i = 1; i < argc;)
-    {
-        std::string key = argv[i];
-        if (0 == key.compare("-h")) /* 帮助 */
-        {
-            printf("USB设备查看工具\n");
-            printf("用法: ./usb_watch [选项]\n");
-            printf("选项: -h    帮助信息\n");
-            printf("      -t    是否树形显示\n");
-            return 0;
-        }
-        else if (0 == key.compare("-t")) /* 树形 */
-        {
-            treeFlag = true;
-            i += 1;
-            continue;
-        }
-    }
+    struct tm t;
+    time_t now;
+    time(&now);
+#ifdef _WIN32
+    localtime_s(&t, &now);
+#else
+    t = *localtime(&now);
+#endif
+    char buf1[20] = {0};
+    strftime(buf1, sizeof(buf1), "%Y-%m-%d %H:%M:%S", &t);
+    char buf2[4] = {0};
+    struct timeb tb;
+    ftime(&tb);
+#ifdef _WIN32
+    sprintf_s(buf2, sizeof(buf2), "%03d", tb.millitm);
+#else
+    sprintf(buf2, "%03d", tb.millitm);
+#endif
+    return std::string(buf1).append(".").append(buf2);
+}
+
+/**
+ * @brief 显示USB设备列表
+ * @param usbList USB设备列表
+ * @param treeFlag 是否树形显示
+ */
+void displayUsbList(const std::vector<std::shared_ptr<usb::Usb>>& usbList, bool treeFlag)
+{
     bool firstLine = true;
     std::string usbListJson;
     usbListJson += "[";
-    auto usbList = usb::Usb::getAllUsbs(true);
     for (size_t i = 0; i < usbList.size(); ++i)
     {
         const auto& info = usbList[i];
@@ -76,5 +85,113 @@ int main(int argc, char** argv)
     }
     usbListJson += "]";
     printf("%s", usbListJson.c_str());
+}
+
+/**
+ * @brief 比较USB设备列表差异
+ * @param preList 之前的USB设备列表
+ * @param nowList 当前的USB设备列表
+ * @param addedList 新增的USB设备列表
+ * @param removedList 移除的USB设备列表
+ */
+void diffUsbList(const std::vector<std::shared_ptr<usb::Usb>>& preList, const std::vector<std::shared_ptr<usb::Usb>>& nowList,
+                 std::vector<std::shared_ptr<usb::Usb>>& addedList, std::vector<std::shared_ptr<usb::Usb>>& removedList)
+{
+    /* 计算新插入 */
+    addedList.clear();
+    for (const auto& now : nowList)
+    {
+        auto iter = std::find_if(preList.begin(), preList.end(), [&](const std::shared_ptr<usb::Usb>& info) {
+            return (now->getPath() == info->getPath() && now->getVid() == info->getVid() && now->getPid() == info->getPid()
+                    && (now->getSerial() == info->getSerial() || now->getAddress() == info->getAddress()));
+        });
+        if (preList.end() == iter)
+        {
+            addedList.emplace_back(now);
+        }
+    }
+    /* 计算已拔出 */
+    removedList.clear();
+    for (const auto& info : preList)
+    {
+        auto iter = std::find_if(nowList.begin(), nowList.end(),
+                                 [&](const std::shared_ptr<usb::Usb>& now) { return (now->getPath() == info->getPath()); });
+        if (nowList.end() == iter)
+        {
+            removedList.emplace_back(info);
+        }
+    }
+}
+
+/**
+ * USB设备查看工具
+ * 用法: ./usb_watch [选项]
+ * 选项: -h 帮助信息
+ *       -t 是否树形显示
+ *       -m 是否实时监听拔插
+ * 例如: ./usb_watch -t
+ */
+int main(int argc, char** argv)
+{
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+    /* 参数值 */
+    bool treeFlag = false;
+    bool monitorFlag = false;
+    /* 解析参数 */
+    for (int i = 1; i < argc;)
+    {
+        std::string key = argv[i];
+        if (0 == key.compare("-h")) /* 帮助 */
+        {
+            printf("USB设备查看工具\n");
+            printf("用法: ./usb_watch [选项]\n");
+            printf("选项: -h    帮助信息\n");
+            printf("      -t    是否树形显示(使用 -m 选项时无法树形显示)\n");
+            printf("      -m    是否实时监听拔插\n");
+            return 0;
+        }
+        else if (0 == key.compare("-t")) /* 树形 */
+        {
+            treeFlag = true;
+            i += 1;
+            continue;
+        }
+        else if (0 == key.compare("-m")) /* 实时监听 */
+        {
+            monitorFlag = true;
+            i += 1;
+            continue;
+        }
+    }
+    s_usbList = usb::Usb::getAllUsbs(true);
+    if (monitorFlag)
+    {
+        while (1)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            auto nowUsbList = usb::Usb::getAllUsbs(true);
+            std::vector<std::shared_ptr<usb::Usb>> addedUsbList, removedUsbList;
+            diffUsbList(s_usbList, nowUsbList, addedUsbList, removedUsbList);
+            s_usbList = nowUsbList;
+            if (!addedUsbList.empty())
+            {
+                printf("[%s] ++++++++++++++++++++ USB设备插入 ++++++++++++++++++++\n", getDateTime().c_str());
+                displayUsbList(addedUsbList, false);
+                printf("\n");
+            }
+            if (!removedUsbList.empty())
+            {
+                printf("[%s] -------------------- USB设备拔出 --------------------\n", getDateTime().c_str());
+                displayUsbList(removedUsbList, false);
+                printf("\n");
+            }
+        }
+    }
+    else
+    {
+        displayUsbList(s_usbList, treeFlag);
+    }
     return 0;
 }
