e42705d13418b373e4aee7c8742a05a1dd47099c
jaronho <892134825@qq.com>
2026-03-31 10:12:19 +0800

    feat: [工具集]修改示例

--- base/cxx/utility/example/example_filecopy.cpp
+++ base/cxx/utility/example/example_filecopy.cpp
@@ -1,3 +1,7 @@
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif
 #include <chrono>
 #include <iostream>
 
@@ -7,6 +11,15 @@
 
 int main(int argc, char** argv)
 {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    /* 关闭控制台程序的快速编辑模式, 否则会出现点击界面, 程序将会变成阻塞状态, 不按回车无法继续运行 */
+    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD mode;
+    GetConsoleMode(hStdin, &mode);
+    mode &= ~ENABLE_QUICK_EDIT_MODE; /* 移除快速编辑模式 */
+    SetConsoleMode(hStdin, mode);
+#endif
     cmdline::parser parser;
     parser.add<std::string>("src", 's', "源目录, 例如: /home/test", true);
     parser.add<std::string>("files", 'f', "指定源目录下要拷贝的文件(全路径), 多个用逗号','分隔, 如果没设置, 则将拷贝源目录所有文件", false);
--- base/cxx/utility/example/example_fileinfo.cpp
+++ base/cxx/utility/example/example_fileinfo.cpp
@@ -1,31 +1,56 @@
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif
 #include <chrono>
 #include <iostream>
 
-#include "../utility/filesystem/file_info.h"
+#include "../utility/filesystem/fs_define.h"
 
 int main(int argc, char** argv)
 {
-    bool firstFlag = true;
-    for (int i = 1; i < argc; ++i)
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    /* 关闭控制台程序的快速编辑模式, 否则会出现点击界面, 程序将会变成阻塞状态, 不按回车无法继续运行 */
+    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD mode;
+    GetConsoleMode(hStdin, &mode);
+    mode &= ~ENABLE_QUICK_EDIT_MODE; /* 移除快速编辑模式 */
+    SetConsoleMode(hStdin, mode);
+#endif
+    if (argc > 1)
     {
-        utility::FileInfo fi(argv[i]);
-        if (fi.exist())
+        for (int i = 1; i < argc; ++i)
         {
-            if (!firstFlag)
+            if (i > 1)
             {
                 printf("--------------------------------------------------\n");
             }
-            auto attr = fi.attribute();
-            printf("        文件: %s\n", fi.name().c_str());
-            printf("    创建时间: %s\n", attr.createTimeFmt().c_str());
-            printf("最后修改时间: %s\n", attr.modifyTimeFmt().c_str());
-            printf("        大小: %zu 字节\n", attr.size);
-            printf("是否链接文件: %s\n", attr.isSymLink ? "是" : "否");
-            printf("是否系统文件: %s\n", attr.isSystem ? "是" : "否");
-            printf("是否隐藏文件: %s\n", attr.isHidden ? "是" : "否");
-            printf("是否可写文件: %s\n", attr.isWritable ? "是" : "否(只读)");
-            printf("  是否可执行: %s\n", attr.isExecutable ? "是" : "否");
+            utility::FileAttribute attr;
+            utility::getFileAttribute(argv[i], attr);
+            if (utility::getFileAttribute(argv[i], attr))
+            {
+                printf("        %s: %s\n", attr.isFile ? "文件" : "目录", argv[i]);
+                printf("    创建时间: %s\n", attr.createTimeFmt().c_str());
+                printf("最后修改时间: %s\n", attr.modifyTimeFmt().c_str());
+                printf("        大小: %zu 字节\n", attr.size);
+                printf("    是否链接: %s\n", attr.isSymLink ? "是" : "否");
+#ifdef _WIN32
+                printf("    是否系统: %s\n", attr.isSystem ? "是" : "否");
+#endif
+                printf("    是否隐藏: %s\n", attr.isHidden ? "是" : "否");
+                printf("    是否可写: %s\n", attr.isWritable ? "是" : "否(只读)");
+                printf("  是否可执行: %s\n", attr.isExecutable ? "是" : "否");
+            }
+            else
+            {
+                printf("目标: %s 不存在\n", argv[i]);
+            }
         }
+    }
+    else
+    {
+        printf("请输入目标, 多个目标用空格分隔\n");
     }
     return 0;
 }
